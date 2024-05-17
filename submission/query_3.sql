ead2b7a8e10515f21e93482dc0e582bbc7c65f6f
lsleena <lsleena@gmail.com>
2024-05-17 14:11:15 -0700

    Modified the SQL

--- submission/query_3.sql
+++ submission/query_3.sql
@@ -4,31 +4,31 @@ WITH yesterday AS
                    SELECT *
                    FROM   lsleena.user_devices_cumulated
                    WHERE  date = date('2022-12-31')
-            )
-            , -- get previous day data
+            ),
             today AS
             (
                      SELECT   user_id,
                               browser_type,
                               count(1)                              AS cnt,
-                              date( cast(event_time AS timestamp) ) AS date_active
+                              date_trunc('day', event_time) AS date_active
                      FROM     bootcamp.web_events we
                      JOIN     bootcamp.devices d
                      ON       we.device_id = d.device_id
-                     WHERE    date( cast(event_time AS timestamp) ) = date('2023-01-01') -- get current day data
+                     WHERE    date_trunc('day', event_time) = date('2023-01-01')
                      GROUP BY 1,
                               2,
                               4
-            )SELECT          COALESCE( yest.user_id, tday.user_id )           AS user_id,
-                   COALESCE( yest.browser_type, tday.browser_type ) AS browser_type,
+            )
+SELECT          COALESCE( yesterday.user_id, today.user_id )           AS user_id,
+                   COALESCE( yesterday.browser_type, today.browser_type ) AS browser_type,
                    CASE
-                                   WHEN yest.dates_active IS NULL THEN array[tday.date_active]
-                                   WHEN tday.date_active IS NULL THEN yest.dates_active
-                                   ELSE array[tday.date_active]
-                                                                   || yest.dates_active
-                   END                                                         AS dates_active, -- based on dates_active between yesterday's dates_active and today dates_active populate date_active
-                   COALESCE( tday.date_active, date_add('day', 1, yest.date) ) AS date
-   FROM            today tday
-   FULL OUTER JOIN yesterday yest
-   ON              tday.user_id = yest.user_id
-   AND             tday.browser_type = yest.browser_type
+                                   WHEN yesterday.dates_active IS NULL THEN array[today.date_active]
+                                   WHEN today.date_active IS NULL THEN yesterday.dates_active
+                                   ELSE array[today.date_active]
+                                                                   || yesterday.dates_active
+                   END                                                         AS dates_active,
+                   COALESCE( today.date_active, date_add('day', 1, yesterday.date) ) AS date
+   FROM            today
+   FULL OUTER JOIN yesterday
+   ON              today.user_id = yesterday.user_id
+   AND             today.browser_type = yesterday.browser_type
